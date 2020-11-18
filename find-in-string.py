be7def47f8672eb5fd62a24245e66e09c026702e
DavidLejeune <Lejeune.David@outlook.com>
2020-11-19 00:52:48 +0100

    find uppercase - recursive , + clear output

--- find-in-string.py
+++ find-in-string.py
@@ -15,8 +15,6 @@ print("String 3: " + input_str_3 + "\n")
 
 
 
-
-
 def find_uppercase_iterative(input_str):
     for i in range(len(input_str)):
         if input_str[i].isupper():
@@ -29,3 +27,19 @@ print("------------------")
 print(find_uppercase_iterative(input_str_1))
 print(find_uppercase_iterative(input_str_2))
 print(find_uppercase_iterative(input_str_3))
+
+print('')
+
+def find_uppercase_recursive(input_str, idx=0):
+    if input_str[idx].isupper():
+        return "String : " + input_str + " > Position : " + str(idx) +  " | Character : " + input_str[idx]
+    if idx == len(input_str) - 1:
+        return "String : " + input_str + " > No uppercase character found"
+    return find_uppercase_recursive(input_str, idx+1)
+
+print("Recursive method :")
+print("------------------")
+
+print(find_uppercase_recursive(input_str_1))
+print(find_uppercase_recursive(input_str_2))
+print(find_uppercase_recursive(input_str_3))
